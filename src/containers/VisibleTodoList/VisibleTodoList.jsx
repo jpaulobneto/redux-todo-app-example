0bb1a50ffaec14f8faa6f3570bab6414c9ffe6b1
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2019-04-04 13:27:38 -0300

    Create VisibleTodoList container

---
+++ src/containers/VisibleTodoList/VisibleTodoList.jsx
@@ -0,0 +1,31 @@
+import { connect } from 'react-redux';
+import { TodoList as Component } from '../../components/TodoList/TodoList';
+import { toggleTodo } from '../actionCreators';
+
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case 'all':
+      return todos;
+    case 'completed':
+      return todos.filter(todo => todo.completed);
+    case 'active':
+      return todos.filter(todo => !todo.completed);
+    default:
+      throw new Error(`Unknown filter ${filter}`);
+  }
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  todos: getVisibleTodos(state.todoApp.todos, ownProps.filter),
+});
+
+const mapDispatchToProps = dispatch => ({
+  onTodoClick(id) {
+    dispatch(toggleTodo(id));
+  },
+});
+
+export const TodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Component);
