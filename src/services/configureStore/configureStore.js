734f59251185343bf59fa9687cbf7d257f30a1a0
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2019-04-04 13:23:38 -0300

    Move rootReducer file to containers folder

--- src/services/configureStore/configureStore.js
+++ src/services/configureStore/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { rootReducer } from '../../rootReducer';
+import { rootReducer } from '../../containers/rootReducer';
 
 export const configureStore = () => {
   const middlewares = [];
