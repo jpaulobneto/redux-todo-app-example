39417b186124ff12faa43b84f7d01355cb550464
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2019-04-04 13:26:20 -0300

    Implement action creators

---
+++ src/containers/actionCreators.js
@@ -0,0 +1,22 @@
+import { v4 } from 'uuid';
+import { ADD_TODO, TOGGLE_TODO } from './actionTypes';
+
+export const addTodo = text => ({
+  type: ADD_TODO,
+  payload: {
+    id: v4(),
+    text,
+  },
+});
+
+export const toggleTodo = id => ({
+  type: TOGGLE_TODO,
+  payload: {
+    id,
+  },
+});
+
+export default {
+  addTodo,
+  toggleTodo,
+};
