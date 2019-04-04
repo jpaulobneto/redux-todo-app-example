45afb0b6f9471991bbf260ec38e6e4e2d303d78c
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2019-04-04 12:08:22 -0300

    Setup store and pass it to the Root component

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
 
-export const Root = () => (
-  <h1>Hello World!</h1>
+export const Root = ({ store }) => (
+  <Provider store={store}>
+    <h1>Hello World!</h1>
+  </Provider>
 );
+
+Root.propTypes = {
+  store: PropTypes.shape({
+    dispatch: PropTypes.func.isRequired,
+    getState: PropTypes.func.isRequired,
+    subscribe: PropTypes.func.isRequired,
+  }).isRequired,
+};
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Root } from './components/Root/Root';
+import { configureStore } from './services/configureStore/configureStore';
 import * as serviceWorker from './services/serviceWorker/serviceWorker';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const store = configureStore();
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
